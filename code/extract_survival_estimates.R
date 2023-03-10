331343f277c5a12c20d78a8ad4d3a54f05a2c786
Mark Wilber <mqwilber@gmail.com>
2023-03-10 11:30:03 -0500

    Remove absolute path

--- code/extract_survival_estimates.R
+++ code/extract_survival_estimates.R
@@ -25,9 +25,8 @@ for(l in 1:length(lake_ids)){
 
 	lake_number = lake_ids[l]
 	cat("Working on lake", lake_number, "\n")
-	basepath = file.path("", "Users", "mqwilber", "Repos")
-	modelpath = file.path(basepath, "cmr-analysis", "out", "model", paste0(lake_number, "_model.rds"))
-	datapath = file.path(basepath, "cmr-analysis", "data", "clean", paste0(lake_number, "_survey.csv"))
+	modelpath = file.path("..", "cmr-analysis", "out", "model", paste0(lake_number, "_model.rds"))
+	datapath = file.path("..", "cmr-analysis", "data", "clean", paste0(lake_number, "_survey.csv"))
 
 	model = readRDS(modelpath)
 	survey_dat = fread(datapath)
