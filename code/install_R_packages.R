df89261920685e65964eed318b064cd24aa5a0c3
Mark Wilber <mqwilber@gmail.com>
2023-03-29 10:23:51 -0400

    Add tmap as dependency

--- code/install_R_packages.R
+++ code/install_R_packages.R
@@ -2,7 +2,7 @@
 
 list.of.packages <- c("data.table", "ggplot2", "patchwork", "cmdstanr", "magrittr",
 					  "rstan", "lubridate", "tidyverse", 
-					  "coda","mvtnorm","devtools","loo","dagitty")
+					  "coda","mvtnorm","devtools","loo","dagitty", "tmap")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos='http://cran.us.r-project.org')
 
